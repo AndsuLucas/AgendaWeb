e98eaf7acf117f947cbc8839ae0081c4e32946e8
Anderson Lucas <44175834+AndsuLucas@users.noreply.github.com>
2019-07-03 06:43:46 -0300

    Update config.php

--- server/config.php
+++ server/config.php
@@ -15,9 +15,9 @@ define ("PATHS",[
 
 //CONFIGURAÇÕES DO BANCO
 define ("DATABASE",[
-	"host"     => "ec2-54-204-35-248.compute-1.amazonaws.com",
-	"port"     => "5432",
-	"dbname"   => "dfvl5p5gq2704k",
-	"user"     => "yjkduhlcljpzqg",
-	"password" => "bbe1208e19d2ecad559c7a6bab6bd53e9f9e5e4597284d395396b6ceb0503da1"
+	"host"     => "",
+	"port"     => "",
+	"dbname"   => "",
+	"user"     => "",
+	"password" => ""
 ]);
